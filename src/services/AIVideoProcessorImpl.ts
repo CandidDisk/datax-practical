11c42fc8d1976621e5b21ab613973d1d75f6af8a
CandidDisk <marktajima@me.com>
2023-06-23 19:57:21 +0800

    feat(AIVideoProcessorImpl.ts): implemented dummy processing & boolean promise return

--- src/services/AIVideoProcessorImpl.ts
+++ src/services/AIVideoProcessorImpl.ts
@@ -1,10 +1,23 @@
-import {AIVideoProcessor} from "./AIVideoProccessor";
+import {AIVideoProcessor} from "./AIVideoProcessor";
 import {Task} from "../models/TaskInterface";
 
 class AIVideoProcessorImpl implements AIVideoProcessor {
-    process(task: Task): boolean {
-        return true;
+    public async process(task: Task): Promise<boolean> {
+        const processTime = task.data.length * 1000
+
+        const result = await dummyProcessing(processTime)
+
+        return result
     }
+
+}
+
+const dummyProcessing = async (processTime: number): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, processTime)
+    })
 }
 
 export { AIVideoProcessorImpl }
