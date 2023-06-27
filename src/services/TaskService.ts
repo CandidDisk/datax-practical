15f99c98651a84e7d7348216eb605b7e7280c044
CandidDisk <marktajima@me.com>
2023-06-28 00:04:57 +0800

    feat(TaskService.ts): getResult returns promise as it now implements redis for in-memory storage. processNextTask is now recursive for retry on failure/

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -4,8 +4,8 @@ interface TaskService {
     enqueue(task: Task): void
     dequeue(): Promise<Task | undefined>
     setResult(taskId: string, result: boolean): Promise<void>
-    getResult(taskId: string): Record<string, boolean> | undefined
-    processNextTask(): Promise<void>
+    getResult(taskId: string): Promise<Record<string, boolean> | undefined>
+    processNextTask(retryCount?: number, retryTask?: Task): Promise<boolean>
 }
 
 export { TaskService }
