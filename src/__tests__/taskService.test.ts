b0d18452efda147c2c2caacbef905ca1fd4fdac8
CandidDisk <marktajima@me.com>
2023-06-28 01:50:18 +0800

    feat(taskService.test.ts): implemented afterEach jest & redis clean up after each test. implemented dequeue test

--- src/__tests__/taskService.test.ts
+++ src/__tests__/taskService.test.ts
@@ -1,9 +1,10 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect, test, afterEach, jest} from '@jest/globals';
 
 import {TaskServiceImpl} from "../services/TaskServiceImpl";
 import {AIVideoProcessorImpl} from "../services/AIVideoProcessorImpl";
 
 import {Task} from "../models/TaskInterface";
+import Redis from "ioredis-mock";
 
 describe('taskService', () => {
     let taskService = new TaskServiceImpl()
@@ -16,28 +17,39 @@ describe('taskService', () => {
             taskId: "1",
             data: "Test 1"
         },
-        {
-            taskId: "2",
-            data: "Test 2"
-        },
     ]
 
-    const redis = new Redis({
+    let redis = new Redis({
         // `options.data` does not exist in `ioredis`, only `ioredis-mock`
         data: {
             taskQueue: []
         },
     })
 
+    // Reset jest mocks & mock redis db
+    afterEach(() => {
+        jest.clearAllMocks()
+        redis.flushall()
+    });
+
     taskService.redisClient = redis
 
+    // Test enqueue single task to redis list
     test('enqueue should enqueue task to redis list', async () => {
-        const task: Task = { taskId: '1', data: 'Test 1' };
+        const task: Task = { taskId: '2', data: 'Test 2' };
 
         await taskService.enqueue(task)
 
         const listLength = await redis.llen(taskService.redisQueueKey)
 
-        expect(listLength).toBeGreaterThan(0)
+        expect(listLength).toEqual(1)
+    })
+
+    // Test dequeue single task from redis list
+    test('dequeue should dequeue single task from redis list', async () => {
+        const expectedTask: Task = { taskId: "1", data: "Test 1" }
+        await redis.rpush("taskQueue", JSON.stringify(expectedTask));
+        const task = await taskService.dequeue()
+        expect(task).toEqual(expectedTask)
     })
 })
